a7127f7f93c1ea2d2d6393955f4eef12d22283a9
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2023-09-24 00:54:27 +0600

    code

--- app.js
+++ app.js
@@ -2,12 +2,129 @@ document.addEventListener("DOMContentLoaded", function () {
     const informationForm = document.getElementById("informationForm");
     const createEntryButton = document.getElementById("createEntry");
     const searchButton = document.getElementById("searchButton");
+    const searchHashCodeButton = document.getElementById("searchHashCodeButton");
     const resultDiv = document.getElementById("result");
     const searchIDInput = document.getElementById("searchID");
 
-    // Replace this with your contract address and ABI
-    const contractAddress = "";
-    const contractABI = []; // Your contract's ABI
+    // Replace with your contract address and ABI
+    const contractAddress = "0xEae4565aa2c1A19733Dc5e02d957e112C1Ae0649";
+    const contractABI = [
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "_informationID",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "string",
+                    "name": "_name",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "_email",
+                    "type": "string"
+                }
+            ],
+            "name": "createInformationEntry",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "informationID",
+                    "type": "uint256"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "bytes32",
+                    "name": "hashCode",
+                    "type": "bytes32"
+                }
+            ],
+            "name": "InformationEntryCreated",
+            "type": "event"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "bytes32",
+                    "name": "_hashCode",
+                    "type": "bytes32"
+                }
+            ],
+            "name": "getInformationByHashCode",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "informationID",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "string",
+                    "name": "name",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "email",
+                    "type": "string"
+                },
+                {
+                    "internalType": "bytes32",
+                    "name": "hashCode",
+                    "type": "bytes32"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "_informationID",
+                    "type": "uint256"
+                }
+            ],
+            "name": "getInformationByInformationID",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "informationID",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "string",
+                    "name": "name",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "email",
+                    "type": "string"
+                },
+                {
+                    "internalType": "bytes32",
+                    "name": "hashCode",
+                    "type": "bytes32"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ]; // Your contract's ABI
 
     const web3 = new Web3(Web3.givenProvider);
     const contract = new web3.eth.Contract(contractABI, contractAddress);
@@ -55,5 +172,39 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error(error);
             resultDiv.innerText = "Error: " + error.message;
         }
+    });
+
+    searchHashCodeButton.addEventListener("click", async () => {
+       // Convert the search hash code to bytes32
+        const searchHashCodeInput = document.getElementById("searchHashCode");
+        const searchHashCode = searchHashCodeInput.value;
+
+        try {
+            // Remove the "0x" prefix if present
+            const cleanSearchHashCode = searchHashCode.startsWith("0x") ? searchHashCode.slice(2) : searchHashCode;
+            
+            // Add "0x" prefix to the cleaned hash code and convert to bytes32
+            const searchHashBytes32 = "0x" + cleanSearchHashCode;
+            
+            // Call the contract to retrieve information by hash code
+            const info = await contract.methods.getInformationByHashCode(searchHashBytes32).call();
+            
+            // Check if the information exists
+            if (info.informationID !== "0") {
+                // If the information ID is found, call the getInformationByInformationID function to get the full information.
+                resultDiv.innerHTML = `
+                    <p>Information ID: ${info.informationID}</p>
+                    <p>Name: ${info.name}</p>
+                    <p>Email: ${info.email}</p>
+                    <p>Hash Code: ${info.hashCode}</p>
+                `;
+            } else {
+                resultDiv.innerText = "Information entry not found for the given hash code.";
+            }
+        } catch (error) {
+            console.error(error);
+            resultDiv.innerText = "Error: " + error.message;
+        }
+
     });
 });
--- main.sol
+++ main.sol
@@ -1,109 +1,79 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+pragma solidity ^0.8.19;
 
+contract Information {
+    struct InformationEntry {
+        uint informationID;
+        string name;
+        string email;
+        bytes32 hashCode;
+    }
 
-contract SampleWallet {
-    // The owner of the wallet
-    address payable public owner;
+    mapping (uint => InformationEntry) informationMapping;
+    mapping (bytes32 => uint) hashCodeToInformationID; // Mapping from hash code to information ID
+    uint[] informationIDs; // To keep track of Information IDs
 
-    // Allowance mapping to track approved amounts for each address
-    mapping(address => uint) public allowance;
+    address contractOwner;
 
-    // Mapping to track whether an address is allowed to send transactions
-    mapping(address => bool) public isAllowedToSend;
+    event InformationEntryCreated(uint informationID, bytes32 hashCode);
 
-    // Mapping to track guardians
-    mapping(address => bool) public guardian;
-
-    // Address of the next proposed owner
-    address payable nextOwner;
-
-    // Count of confirmations received from guardians for owner reset
-    uint guardiansResetCount;
-
-    // Number of confirmations required from guardians for owner reset
-    uint public constant confirmationsFromGuardiansForReset = 3;
+    modifier onlyOwner() {
+        require(msg.sender == contractOwner, "You are not authorized");
+        _;
+    }
 
     constructor() {
-        // Set the contract deployer as the initial owner
-        owner = payable(msg.sender);
+        contractOwner = msg.sender;
     }
 
-    /**
-     * @dev Adds a new guardian to the wallet
-     * @param _guardian The address of the guardian to add
-     */
-    function addGuardian(address _guardian) public {
-        require(msg.sender == owner, "You are not the owner, aborting!");
-        require(!guardian[_guardian], "The address is already a guardian");
+    function createInformationEntry(
+        uint _informationID,
+        string memory _name,
+        string memory _email
+    ) public onlyOwner {
+        bytes32 hashCode = keccak256(abi.encodePacked(_informationID, _name, _email));
+        require(informationMapping[_informationID].informationID == 0, "Information entry already exists");
 
-        guardian[_guardian] = true;
+        informationMapping[_informationID] = InformationEntry({
+            informationID: _informationID,
+            name: _name,
+            email: _email,
+            hashCode: hashCode
+        });
+
+        hashCodeToInformationID[hashCode] = _informationID; // Store the mapping from hash code to information ID
+        informationIDs.push(_informationID);
+        emit InformationEntryCreated(_informationID, hashCode);
     }
 
-    /**
-     * @dev Proposes a new owner for the wallet
-     * @param newOwner The address of the proposed new owner
-     */
-    function proposeNewOwner(address payable newOwner) public {
-        require(guardian[msg.sender], "You are not a guardian, aborting");
-       
-        // Check if the proposed owner is different from the current nextOwner
-        if (nextOwner != newOwner) {
-            nextOwner = newOwner;
-            guardiansResetCount = 0;
-        }
-
-        guardiansResetCount++;
-
-        // If the required number of confirmations is reached, update the owner
-        if (guardiansResetCount >= confirmationsFromGuardiansForReset) {
-            owner = nextOwner;
-            nextOwner = payable(address(0));
-        }
+    function getInformationByInformationID(uint _informationID) public view returns (
+        uint informationID,
+        string memory name,
+        string memory email,
+        bytes32 hashCode
+    ) {
+        InformationEntry storage entry = informationMapping[_informationID];
+        require(entry.informationID != 0, "Information entry not found");
+        
+        informationID = entry.informationID;
+        name = entry.name;
+        email = entry.email;
+        hashCode = entry.hashCode;
     }
 
-    /**
-     * @dev Sets the allowance for a given address
-     * @param _from The address for which to set the allowance
-     * @param _amount The amount to set as the allowance
-     */
-    function setAllowance(address _from, uint _amount) public {
-        require(msg.sender == owner, "You are not the owner, aborting!");
-        allowance[_from] = _amount;
-        isAllowedToSend[_from] = true;
+    function getInformationByHashCode(bytes32 _hashCode) public view returns (
+        uint informationID,
+        string memory name,
+        string memory email,
+        bytes32 hashCode
+    ) {
+        uint _informationID = hashCodeToInformationID[_hashCode];
+        require(_informationID != 0, "Information entry not found for the given hash code");
+
+        InformationEntry storage entry = informationMapping[_informationID];
+        informationID = entry.informationID;
+        name = entry.name;
+        email = entry.email;
+        hashCode = entry.hashCode;
     }
-
-    /**
-     * @dev Denies sending transactions for a given address
-     * @param _from The address for which to deny sending transactions
-     */
-    function denySending(address _from) public {
-        require(msg.sender == owner, "You are not the owner, aborting!");
-        isAllowedToSend[_from] = false;
-    }
-
-
-    /**
-     * @dev Transfers a specified amount to a given address with optional payload
-     * @param _to The address to transfer the funds to
-     * @param _amount The amount to transfer
-     * @param payload The payload data for the transfer
-     * @return The return data from the transfer
-     */
-    function transfer(address payable _to, uint _amount, bytes memory payload) public returns (bytes memory) {
-        require(_amount <= address(this).balance, "Can't send more than the contract owns, aborting.");
-
-        if (msg.sender != owner) {
-            require(isAllowedToSend[msg.sender], "You are not allowed to send any transactions, aborting");
-            require(allowance[msg.sender] >= _amount, "You are trying to send more than you are allowed to, aborting");
-            allowance[msg.sender] -= _amount;
-        }
-
-        (bool success, bytes memory returnData) = _to.call{value: _amount}(payload);
-        require(success, "Transaction failed, aborting");
-        return returnData;
-    }
-
-
-    receive() external payable {}
 }
